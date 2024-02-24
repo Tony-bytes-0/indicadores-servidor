48f1d570d153f8ec27b298599de5cbed25dc76bd
Tony <lonson574gmail.com>
2024-02-24 05:59:40 -0400

    medico

--- src/medico/dto/create-medico.dto.ts
+++ src/medico/dto/create-medico.dto.ts
@@ -1,10 +1,13 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateMedicoDto {
   @IsString()
+  @IsNotEmpty()
   sacs: string;
+  @IsNotEmpty()
   @IsString()
   nombreMedico: string;
+  @IsNotEmpty()
   @IsString()
   especialidad: string;
 }
